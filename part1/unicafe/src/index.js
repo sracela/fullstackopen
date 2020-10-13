ad1127a55cf92aed8c407c11dbb26fec0e471092
sracela <saracelalfonso@gmail.com>
2020-10-14 01:25:01 +0200

    1.7: unicafe step3 (already made in step1 and step2)

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -34,9 +34,7 @@ const Statistics = (props) => {
 
 const App = () => {
   // save clicks of each button to its own state
-  // const [good, setGood] = useState(0)
-  // const [neutral, setNeutral] = useState(0)
-  // const [bad, setBad] = useState(0)
+
 
   const [statistics, setStatistics] = useState([
     {

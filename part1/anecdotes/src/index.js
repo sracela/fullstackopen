d2f2b785acd8db00b8f188b90489d1d9db44b8fe
sracela <saracelalfonso@gmail.com>
2020-10-14 02:40:52 +0200

    1.14*: anecdotes step3

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Header = props => <h1>{props.text}</h1>
+
 const Button = ({ onClick, text }) => (
   <button onClick={onClick}>
     {text}
@@ -9,25 +11,44 @@ const Button = ({ onClick, text }) => (
 
 const Anecdote = props => <p>{props.text} <br /> has {props.votes} votes</p>
 
+const BestAnecdote = ({ clicked, text, votes}) => {
+  if(clicked){
+    return(
+      <div>
+        <Header text="Anecdote with most votes" />
+        <Anecdote text={text} votes={votes} />
+      </div>)
+  } else {
+    return <Header text="No votes for now" />
+  }
+}
+
 const App = (props) => {
   const [selected, setSelected] = useState(0)
+  const [best, setBest] = useState(0)
+  const [clicked, setClicked] = useState(false)
   const [points, setPoints] = useState(Array.apply(null, new Array(props.anecdotes.length)).map(Number.prototype.valueOf,0))
 
   const handleNextAnecdoteClick = () => {
     const random = Math.floor((Math.random() * props.anecdotes.length))
     setSelected(random)
   }
+
   const handleVoteClick = () => {
     const copyPoints = [...points]
     copyPoints[selected] += 1
     setPoints(copyPoints)
+    setClicked(true)
+    setBest(copyPoints.indexOf(Math.max(...copyPoints)))
   }
 
   return (
     <div>
+      <Header text="Anecdote of the day" />
       <Anecdote text={props.anecdotes[selected]} votes={points[selected]} />
       <Button onClick={handleVoteClick} text="vote" />
       <Button onClick={handleNextAnecdoteClick} text="next anecdote" />
+      <BestAnecdote clicked={clicked} text={props.anecdotes[best]} votes={points[best]} />
     </div>
     
   )

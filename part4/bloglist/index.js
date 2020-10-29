41d65b593b5a7e8920a857e37c9d4d92206ce6d6
sracela <saracelalfonso@gmail.com>
2020-10-29 17:19:39 +0100

    4.2 Blog list, step2

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,34 +1,10 @@
-
-/* eslint-disable no-unused-vars */
+const app = require('./app') // the actual Express application
+const http = require('http')
 const config = require('./utils/config')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const blogsRouter = require('./controllers/blogs')
-
 const logger = require('./utils/logger')
 
-const url = config.MONGODB_URI
+const server = http.createServer(app)
 
-logger.info('connecting to', url)
-
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-	.then(result => {
-		logger.info('connected to MongoDB')
-	})
-	.catch((error) => {
-		logger.info('error connecting to MongoDB:', error.message)
-	})
-
-
-app.use(cors())
-app.use(express.json())
-
-
-app.use('/api/blogs', blogsRouter)
-
-const PORT = config.PORT
-app.listen(PORT, () => {
-	logger.info(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+	logger.info(`Server running on port ${config.PORT}`)
 })

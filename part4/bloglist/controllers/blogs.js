fe0c49bbee89b1ec432be086ada3a44913c56559
sracela <saracelalfonso@gmail.com>
2020-11-25 23:49:14 +0100

    4.15: bloglist expansion, step3

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,18 +1,23 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 
 blogsRouter.post('/', async (request, response) => {
 	const body = request.body
+	const user = await User.findById(body.userId)
 
 	const blog = new Blog({
 		title: body.title,
 		author: body.author,
 		url: body.url,
 		likes: body.likes === undefined ? 0 : body.likes,
+		user: user._id
 	})
 
 	const savedBlog = await blog.save()
+	user.blogs = user.blogs.concat(savedBlog._id)
+	await user.save()
 	response.json(savedBlog.toJSON())
 })
 

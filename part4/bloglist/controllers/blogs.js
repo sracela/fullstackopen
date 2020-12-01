4347be2ada57d13087da9508767d44bc30a026d5
sracela <saracelalfonso@gmail.com>
2020-12-01 20:01:44 +0100

    4.19: bloglist expansion, step7

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,11 +1,29 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+/* eslint-disable no-undef */
+require('dotenv').config()
+
+const getTokenFrom = request => {
+	const authorization = request.get('authorization')
+	if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+		return authorization.substring(7)
+	}
+	return null
+}
 
 
 blogsRouter.post('/', async (request, response) => {
 	const body = request.body
-	const user = await User.findById(body.userId)
+	const token = getTokenFrom(request)
+	const decodedToken = jwt.verify(token, process.env.SECRET)
+
+	if (!token || !decodedToken.id) {
+		return response.status(401).json({ error: 'token missing or invalid' })
+	}
+	const user = await User.findById(decodedToken.id)
+	// const user = await User.findById(body.userId)
 
 	const blog = new Blog({
 		title: body.title,

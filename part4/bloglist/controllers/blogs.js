139b93de35f7cac98fa4449c4d2685ed9e73dda4
sracela <saracelalfonso@gmail.com>
2020-12-01 19:43:26 +0100

    4.17: bloglist expansion, step5

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -22,7 +22,7 @@ blogsRouter.post('/', async (request, response) => {
 })
 
 blogsRouter.get('/', async (request, response) => {
-	const blogs = await Blog.find({})
+	const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
 	response.json(blogs)
 	// Blog
 	// 	.find({})
